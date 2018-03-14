7177ada02682402bbb62cdde20320f958c3dc041
Eddie Moon <eddiemoonie@gmail.com>
2018-03-14 21:52:31 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,21 +29,18 @@ class Scraper
         course.schedule = post.css(".date").text
         course.description = post.css("p").text
       end
-
     end
-
-
 
     def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
+      self.make_courses
+      Course.all.each do |course|
+        if course.title
+          puts "Title: #{course.title}"
+          puts "  Schedule: #{course.schedule}"
+          puts "  Description: #{course.description}"
+        end
       end
     end
-  end
 
 end
 
